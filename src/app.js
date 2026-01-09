f50d3101a9804b91e1b6be5b0c20ec74b9cd602f
Suneha Rani <s38414805@gmail.com>
2026-01-09 21:26:38 +0530

    EXploring routing

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express=require("express")
 
 const app=express()
-app.use( "/test",(req,res)=>{
+
+// app.use( "/test",(req,res)=>{
+//     res.send("hello from the  test server")
+// })
+
+app.get( "/ab?c",(req,res)=>{
     res.send("hello from the  test server")
 })
-app.use("/blog",(req,res)=>{
-    res.send("hello from the blog server")
-})
-app.use("/",(req,res)=>{
-    res.send("hello from the server")
-})
+
 
 app.listen(3000,()=>{
     console.log("Server is sucessfully listening from port ")
